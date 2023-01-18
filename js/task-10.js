2eff2a8053441169a91f64cee272d4d838622773
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-01-18 15:14:51 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,6 +3,7 @@ const btnCreate = document.querySelector("button[data-create]");
 const btnDestroy = document.querySelector("button[data-destroy]");
 const numberInput = document.querySelector("#controls").firstElementChild;
 let boxSize = 20;
+let counter = 0;
 
 const createBoxes = () => {
   let amount = Number(numberInput.value.trim());
@@ -13,16 +14,18 @@ const createBoxes = () => {
     boxSize += 10;
     newBox.style.width = `${boxSize}px`;
     newBox.style.height = `${boxSize}px`;
+    numberInput.value = "";
   }
 };
 
-const destroyBoxes = () => {
+const destroyBoxes = (event) => {
   boxes.innerHTML = "";
+  boxSize = 20;
+  numberInput.value = "";
 };
 
 btnCreate.addEventListener("click", createBoxes);
 btnDestroy.addEventListener("click", destroyBoxes);
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
